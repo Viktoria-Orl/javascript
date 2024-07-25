7d106841fe6ac1a52cc44fcb41c4606f760c91fe
Victoria Orleanova <Vika9_96@mail.ru>
2024-07-25 13:43:37 +0300

    change task 1 (space-age)

--- task1_space-age/space-age.js
+++ task1_space-age/space-age.js
@@ -7,35 +7,35 @@ export const age = (planet, sec) => {
   let earthYears = sec / 31557600;
   switch (planet) {
     case 'earth':
-    return +earthYears.toFixed(2);
+    return Number(earthYears.toFixed(2));
       break;
   
     case 'mercury':
-    return +(earthYears / 0.2408467).toFixed(2);
+    return Number((earthYears / 0.2408467).toFixed(2));
       break;
   
     case 'venus':
-    return +(earthYears / 0.61519726).toFixed(2);
+    return Number((earthYears / 0.61519726).toFixed(2));
       break;
   
     case 'mars':
-    return +(earthYears / 1.8808158).toFixed(2);
+    return Number((earthYears / 1.8808158).toFixed(2));
       break;
   
     case 'jupiter':
-    return +(earthYears / 11.862615).toFixed(2);
+    return Number((earthYears / 11.862615).toFixed(2));
       break;
   
     case 'saturn':
-    return +(earthYears / 29.447498).toFixed(2);
+    return Number((earthYears / 29.447498).toFixed(2));
       break;
   
     case 'uranus':
-    return +(earthYears / 84.016846).toFixed(2);
+    return Number((earthYears / 84.016846).toFixed(2));
       break;
   
     case 'neptune':
-    return +(earthYears / 164.79132).toFixed(2);
+    return Number((earthYears / 164.79132).toFixed(2));
       break;
   }
 };
